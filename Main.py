15674e230b5fc5549d1ad42e0b26726832d5545d
Javier Perez Riveros <javier.perezriv@sansano.usm.cl>
2022-08-29 21:59:38 -0400

    added logs (#3)
    Co-authored-by: Javier Pérez <javier.perez@zippedi.com>

--- Main.py
+++ Main.py
@@ -1,52 +1,44 @@
 try:
-    import Stack
-except ImportError:
-    print("Stack.py not found")
-    exit()
-
-try:
     import logging
+    logging.basicConfig( level=logging.DEBUG)
 except ImportError:
     print("Logging.py not found")
     exit()
+try:
+    import Stack
+except ImportError:
+    logging.error("Stack.py not found")
+    exit()
+
 
 if __name__ == "__main__":
     try:
         stack = Stack.Stack()
     except:
-        print("Could not create stack")
+        logging.error("Could not create stack")
     while True:
         try:
             try:
                 choice = int(input("1. Push\n2. Pop\n3. Get Element\n4. Get Largest Element\n5. Compare Items Size\n6. Get Items\n7. Exit\nEnter your choice: "))
                 if choice == 1:
-                    try:
-                        stack.push(str(input("Enter the element to push: ")))
-                    except ValueError:
-                        print("Invalid input")
+                    stack.push(str(input("Enter the element to push: ")))
                 elif choice == 2:
                     stack.pop()
                 elif choice == 3:
-                    try:
-                        print(stack.getElement(int(input("Enter the index: "))))
-                    except ValueError:
-                        print("Invalid input")
+                    print(stack.getElement(int(input("Enter the index: "))))
                 elif choice == 4:
                     print(stack.getLargest())
                 elif choice == 5:
-                    try:
-                        stack.compareItemsSize(int(input("Enter the index 1: ")), int(input("Enter the index 2: ")))
-                    except ValueError:
-                        print("Invalid input")
+                    stack.compareItemsSize(int(input("Enter the index 1: ")), int(input("Enter the index 2: ")))
                 elif choice == 6:
                     stack.getItems()
                 elif choice == 7:
                     exit()
             except ValueError:
-                print("Invalid input")
+                print("Invalid choice")
                 continue
         except KeyboardInterrupt:
-            print("\nExiting...")
+            logging.info("\n Received keyboard interrupt, exiting...")
             break
 else:
-    print("Please run this file directly")
+    logging.error("Please run this file directly")
--- Stack.py
+++ Stack.py
@@ -1,3 +1,5 @@
+import logging
+
 class Stack:
     def __init__(self):
         self.items = []
@@ -5,61 +7,73 @@ class Stack:
     def push(self, item):
         try: 
             self.items.append(item)
+            logging.info(f"Added element to top of stack: {item}")
         except Exception as e:
-            print(e)
+            logging.error("Invalid input")
 
     def pop(self):
         try:
+            logging.info((f"Removed element in top of stack: {self.getElement[-1]}"))
             self.items.pop()
         except IndexError:
-            print("Stack is empty")
+            logging.error("Stack is empty")
         return
     
     def getElement(self, index):
+        if self.isEmpty():
+            logging.warning("Stack is empty")
         try:
+            logging.info(f"Returning element at index {index}: {self.items[index]}")
             return self.items[index]
         except IndexError:
-            print("Stack is empty")
+            logging.error("index out of range")
             return
     
     def isEmpty(self):
         return self.items == []
     
     def getItems(self):
+        if self.isEmpty():
+            logging.warning("Stack is empty")
+            return
         for i in reversed(self.items):
             print(f"[{self.items.index(i)}]. {i} \n")
+        return
     
     def getLargest(self):
         if self.isEmpty():
-            print("Stack is empty")
+            logging.warning("Stack is empty")
             return
         else:
             return max(self.items, key=len)
     
     def getItemSize(self, item):
         if self.isEmpty():
-            print("Stack is empty")
+            logging.error("Stack is empty")
             return
         try:
             return len(item)
         except TypeError:
-            print("Invalid input, not a string")
+            logging.error("Invalid input, not a string")
             return
     
     def compareItemsSize(self, index1, index2):
         try:
             if self.getItemSize(self.getElement(index1)) > self.getItemSize(self.getElement(index2)):
                 print(f"Item 1 ({self.getElement(index1)}) is larger")
+                logging.info(f"Input 1 {index1}, input 2 {index2}, result {self.getElement(index1)} is larger")
             elif self.getItemSize(self.getElement(index1)) < self.getItemSize(self.getElement(index2)):
+                logging.info("Input 1 {index1}, input 2 {index2}, result {self.getElement(index2)} is larger")
                 print(f"Item 2 ({self.getElement(index2)}) is larger")
             else:
+                logging.info("Input 1 {index1}, input 2 {index2}, result {self.getElement(index1)} is equal")
                 print("Items are equal")
         except ValueError:
-            print("Invalid input")
+            logging.error("Invalid input")
             return
         except IndexError:
-            print("Index Out of Range")
+            logging.error("Index out of range")
             return
         except TypeError:
-            print("Invalid input, not a string")
+            logging.error("Invalid input, not a string")
             return
